56efab05591d4f65687cb0385364f02c87ca63bb
Laurohms <laurohmsantos@gmail.com>
2025-02-17 11:54:03 -0300

    Project Created

---
+++ src/main/kotlin/tech/laurosantos/lbveiculosapi/LbVeiculosApiApplication.kt
@@ -0,0 +1,11 @@
+package tech.laurosantos.lbveiculosapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LbVeiculosApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<LbVeiculosApiApplication>(*args)
+}
---
+++ src/test/kotlin/tech/laurosantos/lbveiculosapi/LbVeiculosApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.laurosantos.lbveiculosapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LbVeiculosApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
